609d8d9de93d117f6c1a95b033becbeb93502f78
Jeff Boek <jeff@jeffboek.com>
2020-05-15 10:18:44 -0700

    Adds Fab Icon

--- app/src/main/java/org/mozilla/fenix/tabtray/TabTrayView.kt
+++ app/src/main/java/org/mozilla/fenix/tabtray/TabTrayView.kt
@@ -4,14 +4,17 @@
 
 package org.mozilla.fenix.tabtray
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import kotlinx.android.extensions.LayoutContainer
 import kotlinx.android.synthetic.main.component_tabstray.view.*
+import kotlinx.android.synthetic.main.component_tabstray_fab.view.*
 import kotlinx.android.synthetic.main.fragment_tab_tray.*
 import mozilla.components.browser.tabstray.BrowserTabsTray
+import mozilla.components.concept.tabstray.Tab
 import mozilla.components.feature.tabs.tabstray.TabsFeature
 import mozilla.components.support.base.feature.UserInteractionHandler
 import org.mozilla.fenix.R
@@ -22,7 +25,8 @@ import org.mozilla.fenix.ext.components
  * to respond to user interaction on the AwesomebarView
  */
 interface TabTrayInteractor {
-
+//    fun onTabSelected(tab: Tab)
+//    fun onNewTabTapped()
 }
 
 /**
@@ -32,6 +36,9 @@ class TabTrayView(
     private val container: ViewGroup,
     val interactor: TabTrayInteractor
 ) : LayoutContainer, UserInteractionHandler {
+    val fabView = LayoutInflater.from(container.context)
+        .inflate(R.layout.component_tabstray_fab, container, true)
+
     val view = LayoutInflater.from(container.context)
         .inflate(R.layout.component_tabstray, container, true)
 
@@ -41,9 +48,24 @@ class TabTrayView(
     override val containerView: View?
         get() = container
 
-
     init {
+        fabView.new_tab_button.compatElevation = 80.0f
         behavior.state = BottomSheetBehavior.STATE_HIDDEN
+        behavior.addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+                Log.e("slideOffset", "$slideOffset")
+                if (slideOffset > 0) {
+                    fabView.new_tab_button.show()
+                } else {
+                    fabView.new_tab_button.hide()
+                }
+            }
+
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+
+            }
+
+        })
 
         tabsFeature = TabsFeature(
             view.tabsTray,
